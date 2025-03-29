9bde547cb2e9ef6000ae51d4c0dd408399d4e275
Tuan Linh Nguyen <52875747+silentlie@users.noreply.github.com>
2025-03-29 14:25:56 +1000

    remove .env as not working as expected

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -14,8 +13,6 @@ import 'package:adsats_amplify_gen_2/models/ModelProvider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  print(dotenv.env["BASE_URL"]);
   await configureAmplify();
   setPathUrlStrategy();
   GoRouter.optionURLReflectsImperativeAPIs = true;
