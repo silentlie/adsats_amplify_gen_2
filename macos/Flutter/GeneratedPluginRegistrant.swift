a940a4cd080ad4c87c37333f1aeb18860ec51928
Linh Tuan Nguyen <52875747+silentlie@users.noreply.github.com>
2026-02-21 12:50:37 +1000

    chore: update Amplify/AWS/Dart deps and regenerate Windows plugin registrant
    Updated Node devDependencies and AWS/Amplify packages in package.json, refreshed Dart/Flutter package versions in pubspec.lock after dependency upgrades, and regenerated Windows plugin registrant and CMake plugin list to reflect the updated plugins.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import connectivity_plus
 import device_info_plus
 import file_picker
 import package_info_plus
-import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
